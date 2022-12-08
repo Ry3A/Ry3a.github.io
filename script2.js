dea629cbeb5ae91e0f5a20ba214617b7e276b3e9
apl26 <apl2612@mail.ru>
2022-12-08 04:33:42 +0300

    kurs

--- script2.js
+++ script2.js
@@ -1,7 +1,8 @@
-let str2 = document.getElementById("box123").textContent;
+/*let str2 = document.getElementById("box123").textContent;
 alert(str2 + "!!!");
 function truncate(str, maxlength= 50) {
     return (str.length > maxlength) ?
         str.slice(0, maxlength - 1) + '…' : str;
 }
 document.getElementById("box123").textContent = truncate(str2);
+*/
